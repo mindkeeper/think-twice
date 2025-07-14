9ffd91fe6e383e931522c0497c4e9d76047b1707
ggndesign <ggndesign.work@gmail.com>
2025-07-15 00:02:39 +0700

    Merge branch 'main' into feat/post-creation and resolve conflicts

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 "use client";
 
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
+
 import { cn } from "@/lib/utils";
 
 function Label({ className, ...props }) {
-=======
-"use client"
-
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-
-import { cn } from "@/lib/utils"
-
-function Label({
-  className,
-  ...props
-}) {
->>>>>>> main
   return (
     <LabelPrimitive.Root
       data-slot="label"
@@ -26,17 +13,9 @@ function Label({
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
-<<<<<<< HEAD
       {...props}
     />
   );
 }
 
 export { Label };
-=======
-      {...props} />
-  );
-}
-
-export { Label }
->>>>>>> main
