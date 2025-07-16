7bda86f4f3e6fda9325a9965a3d7fab3c3213e13
ggndesign <ggndesign.work@gmail.com>
2025-07-16 14:40:20 +0700

    feat/post-card: Implement PostCard component UI

--- src/app/(app)/page.jsx
+++ src/app/(app)/page.jsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/utils/prisma";
-import Link from "next/link"; // Import Link component for client-side navigation
-import PostList from "./post-creation/_components/PostList";
+import Link from "next/link";
+import PostCard from "./post-creation/_components/PostCard";
 import { Button } from "@/components/ui/button";
 
 export default async function HomePage() {
@@ -18,25 +18,30 @@ export default async function HomePage() {
     posts = posts.map((post) => {
       const postForClient = {
         ...post,
-        price: post.price.toNumber(),
+        price: post.price.toString(),
         createdAt: post.createdAt.toISOString(),
         updatedAt: post.updatedAt.toISOString(),
       };
 
-      if (postForClient.user) {
+      if (post.user) {
         postForClient.user = {
-          id: postForClient.user.id,
-          email: postForClient.user.email,
-          name: postForClient.user.name,
+          id: post.user.id,
+          email: post.user.email,
+          name: post.user.name,
+          avatarUrl: post.user.avatarUrl,
         };
+      } else {
+        postForClient.user = null;
       }
 
-      if (postForClient.category) {
+      if (post.category) {
         postForClient.category = {
-          id: postForClient.category.id,
-          name: postForClient.category.name,
-          slug: postForClient.category.slug,
+          id: post.category.id,
+          name: post.category.name,
+          slug: post.category.slug,
         };
+      } else {
+        postForClient.category = null;
       }
 
       return postForClient;
@@ -46,24 +51,25 @@ export default async function HomePage() {
   }
 
   return (
-    <div className="card min-h-screen flex flex-col justify-center items-center shadow-md border rounded-md w-full mx-auto">
-      {posts.length && (
-        <div className="px-16 flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold mb-8 text-center">
-            Welcome to Think Twice
-          </h1>
-          <p className="text-lg text-gray-700 mb-8 text-center max-w-prose">
-            Your platform to discuss products before you make a decision to buy
-            or skip.
-          </p>
-          <Button size="lg" asChild>
-            <Link href="/post-creation">Create a New Post</Link>
-          </Button>
-        </div>
-      )}
+    <div className="card min-h-screen flex flex-col justify-center items-center w-full mx-auto">
+      {/* <div className="px-16 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          Welcome to Think Twice
+        </h1>
+        <p className="text-lg text-gray-700 mb-8 text-center max-w-prose">
+          Your platform to discuss products before you make a decision to buy or
+          skip.
+        </p>
+        <Button size="lg" asChild>
+          <Link href="/post-creation">Create a New Post</Link>
+        </Button>
+      </div> */}
 
-      <section className="w-full">
-        <PostList posts={posts} />
+      <section className="w-full space-y-2">
+        {posts.length > 0 &&
+          posts.map((singlePost) => (
+            <PostCard key={singlePost.id} post={singlePost} />
+          ))}
       </section>
     </div>
   );
