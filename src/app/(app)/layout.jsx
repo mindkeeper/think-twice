fe9e56593c41c614e67b90248d4fb040c88c1607
ggndesign <ggndesign.work@gmail.com>
2025-07-18 09:18:03 +0700

    feat/top-app-bar: Implemented dynamic top app bar

--- src/app/(app)/layout.jsx
+++ src/app/(app)/layout.jsx
@@ -5,7 +5,7 @@ export default async function AppLayout({ children }) {
   return (
     <div className="flex-1 relative min-h-screen w-full max-w-lg mx-auto">
       <TopAppBar />
-      <main className="pb-16 pt-14">{children}</main>
+      <main className="pb-16">{children}</main>
       <MobileNav />
     </div>
   );
